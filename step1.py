a5e8770ec24f623bc0f2a200f4f225d6c49b9094
Alex Suspiro <alexandresuspiro@gmail.com>
2021-11-10 12:43:42 +0000

    add function pseudocode to step1.py

---
+++ step1.py
@@ -0,0 +1,20 @@
+#Extract the SIFT features from the training and the test images
+
+#Implement the SIFT descriptor from scratch, we are not allowed to call inbuilt SIFT functions from other libraries such as OpenCV
+
+
+#Input -> directory path (where the images we want are)
+#Output -> list/array of images
+
+#def load_images(path):
+
+
+
+
+
+#Input -> image
+#Output -> image's list of SIFT features
+#def sift(img):
+
+
+
