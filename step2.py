d6fa7d08595f3d64fa5ea391e2341e92fcb8a57e
Alex Suspiro <alexandresuspiro@gmail.com>
2021-11-12 12:14:24 +0000

    add step2 and step 3

---
+++ step2.py
@@ -0,0 +1,11 @@
+#Input: List of SIFT Descriptors, number of words n
+#Output: a dictionary of n words
+def generate_dictionary(descriptors, n):
+    #cluster the descriptors 
+    #implement kmeans algorithm
+    
+    pass
+
+
+
+
---
+++ step3.py
@@ -0,0 +1,5 @@
+#Image representation with a histogram of codewords
+
+#Calculate Euclidian distance between image descriptors and codewords (codewords are cluster centers)
+
+#:
